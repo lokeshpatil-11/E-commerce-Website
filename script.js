4bfeb667a50d0cb30d707adf68182d850c9de2f6
Lokesh Patil <patillokesh1105@gmail.com.com>
2023-02-19 16:41:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+}
+
+
+// cart section
+
+// const product = [
+//     {
+//         id: 0,
+//         Image: 'img/products/f1.jpg',
+//         title: "Men's Fashion T shirt",
+//         price: 139,
+//     },
+//     {
+//         id: 1,
+//         Image: 'img/products/f2.jpg',
+//         title: "Men's Fashion T shirt",
+//         price: 139,
+//     }
+// ];
+
+// const categories = [...new Set(product.map((item)=>
+//     {return item}))]
+//     let i = 0;
+
+// document.getElementById('prodetails').innerHTML = categories.map((item)=>
+// {
+//     var {image, title, price} = item;
+//     return(
+//         `<div class='box'>
+//             <div class='img-box'>
+//                 <img class='images' src=${image}></img>
+//                 </div>
+//             <div class='bottom'>
+//             <p>$(title)</p>
+//             <h2>$ $(price).00</h2>` +
+//             "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+//             `</div>
+//             </div>`
+//     )
+// }).join('')
+
+// var cart= [];
+
+// function addtocart(a){
+//     cart.push({...categories[a]});
+//     displaycart();
+// }
+
+// function displaycart(a){
+//     let j = 0;
+//     if(cart.length==0){
+//         document.getElementById('cartItem').innerHTML = "Your cart is empty";
+//     }
+//     else{
+//         document.getElementById('cartItem').innerHTML = cart.map((items)=>
+//         {
+//             var {image, title, price} = items;
+//             return(
+//                 `<div class='cart-item'>
+//                 <div class= 'row-img'>
+//                     <img class = 'rowimg' src= ${image}>
+//                 </div>
+//                 <p style= 'font-size:12px;'>${title}</p>
+//                 <h2 style='font-size: 15px;'>$ ${price}.00</h2>` +
+//                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+//             );
+//         }).join('');
+//     }
+// }
